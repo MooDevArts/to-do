8997ef1818c8e6ee27d939f5c98a1df67274ab26
MooDevArts <himanshu.a.04.1999@gmail.com>
2024-01-12 16:53:05 +0530

    Updates

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Hi</h1>');
+}
+
+module.exports.lol = function(req, res){
+    return res.end('<h1>lol</h1>');
+}
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use router (defined in routes -> indexjs)
+const router = require('./routes');
+app.use('/', router);
+
 app.listen(port, function(err){
     if (err){
         console.log(`Error: ${err}`);
